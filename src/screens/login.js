cdbb1f69244336b2033a022f162341995a4db4fa
Eduardo-Fari <eduardofariadesantana12@gmail.com>
2026-08-10 11:23:54 -0300

    modificações no login

--- src/screens/login.js
+++ src/screens/login.js
@@ -5,41 +5,49 @@ import React from 'react'
 
 const LoginScreen = ({navigation}) => {
   const [Email, setEmail] = useState();
-  const [senha, setSenha] = useState(true);
+  const [Senha, setSenha] = useState();
+  const [Secure, setSecure] = useState(true);
   return (
-    <View>
-      <View>
-        <Image source={require('../../assets/IconLogin.png')} style={styles.Logo} />
-      </View>
-      <View>
-      <Text styles={style.textHeader1}>faça login</Text>
-      <TextInput
-            style={styles.input}
-            placeholder='Email'
-            onChangeText={setEmail}
-            placeholderTextColor="#ccc"
-            value={Email}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder='Senha'
-            onChangeText={setSenha}
-            placeholderTextColor="#ccc"
-            type="password"
-            secureTextEntry={secure}
-            value={Senha}
-          />
-      <TouchableOpacity onPress={() => {setSecure(!secure)}}></TouchableOpacity>
-      </View>
-      <View>
-      <Button title='Login' onPress={()=>{
-              navigation.goBack();
-      }}/>
-      
-      <Text>Não possui Conta?</Text>
-      <Button title='Criar Conta' onPress={()=>{
-            navigation.navigate('Home');
-      }}/>
+    <View styles={style.Container}>
+      <View styles={style.borda}>
+        <View styles={style.imagem}>
+          <Image source={require('../../assets/IconLogin.png')} style={styles.Logo} />
+        </View>
+        <View>
+        <Text styles={style.textHeader1}>faça login</Text>
+        <TextInput
+              style={styles.input}
+              placeholder='Email'
+              onChangeText={setEmail}
+              placeholderTextColor="#ccc"
+              value={Email}
+            />
+            <View>
+            <TextInput
+              style={styles.input}
+              placeholder='Senha'
+              onChangeText={setSenha}
+              placeholderTextColor="#ccc"
+              type="password"
+              secureTextEntry={secure}
+              value={Senha}
+            />
+        <TouchableOpacity onPress={() => {setSecure(!secure)}}></TouchableOpacity>
+        </View>
+        </View>
+        <View>
+        <Button title='Login' onPress={()=>{
+                navigation.goBack();
+        }}/>
+        
+        <Text>Não possui Conta?</Text>
+        <Button title='Criar Conta' onPress={()=>{
+              navigation.navigate('Home');
+        }}/>
+        </View>
+        <View style={styles.icone}>
+          <Image source={require('../../assets/IconLogin.png')} style={styles.Logo2} />
+        </View>
       </View>
     </View>
   )
